1a7df54e3409942416ae66455cac3582cffb9e0f
student200036 <200036ww@gmail.com>
2021-12-03 09:13:28 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+const host = process.env.HOST
+const port = process.env.PORT
+const default_login_name = process.env.LOGIN_NAME
+const default_password = process.env.PASSWORD
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/auth', (req, res) => {
+    let message = 'ログインでき...ないィ！？'
+    const login_name = req.body.login_name
+    const password = req.body.password
+    console.log(login_name)
+    console.log(password)
+
+    if(login_name == default_login_name
+        && password == default_password){
+            message = 'ログインしました'
+        }
+    res.send(message)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello YSE!!!!')
+})
+
+app.get('/profile', (req, res) => {
+    res.send('ここはprofileのページやで')
+})
+
+app.listen(port, host, () => {
+    console.log('http://' + host + ':' + port)
+})
