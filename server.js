6ef0c4b0bbef8f5dd0d407750a06eab11c500328
student200036 <200036ww@gmail.com>
2021-12-06 13:55:15 +0900

    commit

--- server.js
+++ server.js
@@ -10,16 +10,15 @@ const routes = require('./routes')
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
-
 app.use(express.urlencoded({ extended: true }))
-
-app.use(routes)
 
 const layouts = require('express-ejs-layouts')
 app.set('layout', 'layouts/default');
 app.use(layouts);
 
 app.set('view engine', 'ejs')
+
+app.use(routes)
 
 app.listen(port, host, () => {
     console.log('http://' + host + ':' + port)
